a732d1bd2fbecb5089711c9660d08a8c08770f35
Tadas Gricius <griciustadas@gmail.com>
2020-09-27 20:04:26 +0200

    rendering individual expenses

--- public/src/app.js
+++ public/src/app.js
@@ -11,13 +11,13 @@ import './styles/styles.sass';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill'}));
+store.dispatch(addExpense({ description: 'Water Bill', amount: 4500}));
 store.dispatch(addExpense({ description: 'Gas Bill'}));
 store.dispatch(setTextFilter('Gas'));
 
 
 setTimeout(() => {
-  store.dispatch(setTextFilter('rent'))
+  store.dispatch(setTextFilter('bill'))
 }, 3000);
 
 const state = store.getState();
