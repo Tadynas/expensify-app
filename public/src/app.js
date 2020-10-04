51546ca75cad5383898feed4057b0d7db8997e06
Tadas Gricius <griciustadas@gmail.com>
2020-10-04 13:22:32 +0200

    dropdown sortby

--- public/src/app.js
+++ public/src/app.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.sass';
@@ -12,13 +11,8 @@ import './styles/styles.sass';
 const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water Bill', amount: 4500}));
-store.dispatch(addExpense({ description: 'Gas Bill'}));
-store.dispatch(setTextFilter('Gas'));
-
-
-setTimeout(() => {
-  store.dispatch(setTextFilter('bill'))
-}, 1000);
+store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000}));
+store.dispatch(addExpense({ description: 'Rent', amount: 29000, createdAt: 500}));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
