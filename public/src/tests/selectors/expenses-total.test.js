82e2f8ef2868fa90f3972b66fed7d7ebd7193079
Tadas Gricius <griciustadas@gmail.com>
2020-11-03 23:33:25 +0200

    Adding summary component

--- public/src/tests/selectors/expenses-total.test.js
+++ public/src/tests/selectors/expenses-total.test.js
@@ -2,16 +2,16 @@ import selectExpensesTotal from '../../selectors/expenses-total';
 import expenses from '../fixtures/expenses';
 
 test('Should return 0 of no expenses', () => {
-  const result = selectExpensesTotal([]);
+  const result = expensesTotal([]);
   expect(result).toBe(0);
 });
 
 test('Should correctly add up a single expense', () => {
-  const result = selectExpensesTotal([ expenses[0] ]);
+  const result = expensesTotal([ expenses[0] ]);
   expect(result).toBe(expenses[0].amount);
 });
 
 test('Should correctly add up a multiple expenses expense', () => {
-  const result = selectExpensesTotal(expenses);
+  const result = expensesTotal(expenses);
   expect(result).toBe(expenses[0].amount + expenses[1].amount + expenses[2].amount);
 });
