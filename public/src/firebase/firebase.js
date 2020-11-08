6a1279fdd90d039fc48c782bccbcecd44166afe7
Tadas Gricius <griciustadas@gmail.com>
2020-11-08 17:38:21 +0200

    Fetching data from firebase

--- public/src/firebase/firebase.js
+++ public/src/firebase/firebase.js
@@ -14,33 +14,67 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-database.ref().set({
-  name: 'Gret',
-  age: 21,
-  stressLevel: 99,
-  job: {
-    title: 'Baker',
-    company: 'Gwet Baking Company'
-  },
-  location: {
-    city: 'Vilnius',
-    country: 'Lithuania'
-  },
-}).then(() => {
-  console.log('1. Data is saved');
-}).catch((error) => {
-  console.log('1. This failed', error);
+// database.ref('location/city')
+//   .once('value')
+//   .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch((error) => {
+//     console.log('Error fetching data', error);
+// });
+
+database.ref().on('value', (snapshot) => {
+  const { name, job: { title, company }} = snapshot.val();
+  console.log(`${name} is a ${title} at ${company}.`);
 });
 
-database.ref().update({
-  stressLevel: 101,
-  'job/company': 'Greta Reta Baking Company',
-  'location/country': 'Amsterdam'
-}).then(() => {
-  console.log('Data is updated');
-}).catch((error) => {
-  console.log('Update failed', error);
-});
+
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//   console.log(snapshot.val());
+// }, (error) => {
+//   console.log('Error with data fetching: ', error)
+// });
+
+// setTimeout(() => {
+//   database.ref('age').set(420);
+// }, 3500);
+
+// setTimeout(() => {
+//   database.ref().off(onValueChange);
+// }, 7000);
+
+// setTimeout(() => {
+//   database.ref('age').set(999);
+// }, 10500);
+
+// database.ref().set({
+//   name: 'Gret',
+//   age: 21,
+//   stressLevel: 99,
+//   job: {
+//     title: 'Baker',
+//     company: 'Gwet Baking Company'
+//   },
+//   location: {
+//     city: 'Vilnius',
+//     country: 'Lithuania'
+//   },
+// }).then(() => {
+//   console.log('1. Data is saved');
+// }).catch((error) => {
+//   console.log('1. This failed', error);
+// });
+
+// database.ref().update({
+//   stressLevel: 101,
+//   'job/company': 'Greta Reta Baking Company',
+//   'location/country': 'Amsterdam'
+// }).then(() => {
+//   console.log('Data is updated');
+// }).catch((error) => {
+//   console.log('Update failed', error);
+// });
 
 // database.ref('isSingle').set(null);
 
