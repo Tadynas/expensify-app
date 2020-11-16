431410664b8dcb8c398a1f1ce10bea9d7b7901a1
Tadas Gricius <griciustadas@gmail.com>
2020-11-16 11:52:08 +0200

    Setup database environment

--- public/src/firebase/firebase.js
+++ public/src/firebase/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBWBoKC64295kBfzvshW5sRc5H2FKRAU0M",
-  authDomain: "expensify-app-3bc4a.firebaseapp.com",
-  databaseURL: "https://expensify-app-3bc4a.firebaseio.com",
-  projectId: "expensify-app-3bc4a",
-  storageBucket: "expensify-app-3bc4a.appspot.com",
-  messagingSenderId: "360933236368",
-  appId: "1:360933236368:web:bbbfd02c7248d3235a8215"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
